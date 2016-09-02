861bbfe1b323f6ac267b4a22cc7b0e8b8fd0312f
Ty Gooch <gooch.ty@gmail.com>
2016-09-02 09:38:20 -0700

    Updated styling

--- frontend/components/explore/explore.jsx
+++ frontend/components/explore/explore.jsx
@@ -2,14 +2,28 @@ import React from 'react';
 
 const exploreHeader = () => (
   <div className="explore-header">
-    <img src='http://res.cloudinary.com/dtatkxxcm/image/upload/v1472669919/exploreHeader_ilzyq7.png'
-      className="explore-header-image" />
+    <div className ='explore-header-text'>EXPLORE</div>
   </div>
 );
 
 const Explore = () => (
   <div className = 'explore-comtainer'>
     {exploreHeader()}
+    <div className='explore-content-container'>
+    <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSkbGJYQxY1uD_fi8Ot7LIbs29xPzzCLCqgjdDUELevGOs36ngg' /></div>
+    <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSkbGJYQxY1uD_fi8Ot7LIbs29xPzzCLCqgjdDUELevGOs36ngg' /></div>
+    <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSkbGJYQxY1uD_fi8Ot7LIbs29xPzzCLCqgjdDUELevGOs36ngg' /></div>
+    <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSkbGJYQxY1uD_fi8Ot7LIbs29xPzzCLCqgjdDUELevGOs36ngg' /></div>
+    <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSkbGJYQxY1uD_fi8Ot7LIbs29xPzzCLCqgjdDUELevGOs36ngg' /></div>
+    <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSkbGJYQxY1uD_fi8Ot7LIbs29xPzzCLCqgjdDUELevGOs36ngg' /></div>
+    <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSkbGJYQxY1uD_fi8Ot7LIbs29xPzzCLCqgjdDUELevGOs36ngg' /></div>
+    <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSkbGJYQxY1uD_fi8Ot7LIbs29xPzzCLCqgjdDUELevGOs36ngg' /></div>
+    <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSkbGJYQxY1uD_fi8Ot7LIbs29xPzzCLCqgjdDUELevGOs36ngg' /></div>
+    <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSkbGJYQxY1uD_fi8Ot7LIbs29xPzzCLCqgjdDUELevGOs36ngg' /></div>
+    <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSkbGJYQxY1uD_fi8Ot7LIbs29xPzzCLCqgjdDUELevGOs36ngg' /></div>
+    <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSkbGJYQxY1uD_fi8Ot7LIbs29xPzzCLCqgjdDUELevGOs36ngg' /></div>
+    <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSkbGJYQxY1uD_fi8Ot7LIbs29xPzzCLCqgjdDUELevGOs36ngg' /></div>
+  </div>
   </div>
 );
 
