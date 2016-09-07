35f69f0521256e743c34ab91adc837bf0f835208
Ty Gooch <gooch.ty@gmail.com>
2016-09-06 17:29:59 -0700

    Fixed album page

--- frontend/components/album/album.jsx
+++ frontend/components/album/album.jsx
@@ -9,18 +9,18 @@ class Album extends React.Component {
       const albumSongs = this.props.artist.songs.filter(song => song.album_id === this.props.albumId);
       const otherAlbums = this.props.artist.albums.filter(el => el.id !== this.props.albumId);
       return(
-        <div>
+        <div className = "album-show-container">
           <ArtistHeader artist = {this.props.artist} />
           <div className = 'album-container'>
             <div className = 'album-content-container'
             >
               <div className = 'album-header'>
-                <div  >
-                  <img src={this.props.album[0].album_picture_url}
-                    className= 'album-header-image'/>
-                </div>
                 <div className = 'album-header-name'>
                   {this.props.album[0].title}
+                </div>
+                <div>
+                  <img src={this.props.album[0].album_picture_url}
+                    className= 'album-header-image'/>
                 </div>
               </div>
               <div className = 'album-song-index'>
@@ -44,27 +44,3 @@ class Album extends React.Component {
   }
 }
 export default Album;
-// <ArtistHeader artist = {this.props.artist} />
-// <div className = 'album-container'>
-//   <div className = 'album-content-container'
-//   >
-//     <div className = 'album-header'>
-//       <div  >
-//         <img src={this.props.album[0].album_picture_url}
-//           className= 'album-header-image'/>
-//       </div>
-//       <div className = 'album-header-name'>
-//         {this.props.album[0].title}
-//       </div>
-//     </div>
-//     <div className = 'album-song-index'>
-//       <SongIndex songs={albumSongs} />
-//     </div>
-//   </div>
-//   <div className = 'album-sidebar-index-container'>
-//     <div className = 'album-sidebar-index-header'>
-//       More by {this.props.artist.name}
-//     </div>
-//     <AlbumIndex albums = {otherAlbums} />
-//   </div>
-// </div>
