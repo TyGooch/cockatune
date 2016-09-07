370eb32a23bc08b99d7e10ab0abad1e54158f5ec
Ty Gooch <gooch.ty@gmail.com>
2016-09-06 19:14:53 -0700

    Updated styling

--- frontend/components/song/song_index_item.jsx
+++ frontend/components/song/song_index_item.jsx
@@ -5,11 +5,13 @@ import SongContainer from './song_container';
 const SongIndexItem = ({song}) => (
   <li className = 'song-index-item'>
     <SongContainer id = {song.id} song = {song} />
-    <div className = 'song-index-item-title'>
-        {song.title}
-    </div>
-    <div className = 'song-length'>
-      {Math.floor(song.length / 60)}:{song.length % 60}
+    <div className = 'song-index-item-info'>
+      <div className = 'song-index-item-title'>
+          {song.title}
+      </div>
+      <div className = 'song-length'>
+        {Math.floor(song.length / 60)}:{song.length % 60}
+      </div>
     </div>
   </li>
 );
