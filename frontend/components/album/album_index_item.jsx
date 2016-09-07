6742a3f84930a5c31d9ff2ed13effb13f462bdc1
Ty Gooch <gooch.ty@gmail.com>
2016-09-07 07:50:56 -0700

    Updated styling

--- frontend/components/album/album_index_item.jsx
+++ frontend/components/album/album_index_item.jsx
@@ -3,13 +3,13 @@ import { Link, hashHistory } from 'react-router';
 
 const AlbumIndexItem = ({album}) => (
   <li className = 'album-index-item'>
-      <Link to={`/artists/${album.artist_id}/albums/${album.id}`}>
-        <img className = 'album-index-item-image'
-      src={album.album_picture_url}
-    /></Link>
-  <div className = 'album-index-item-title'>
-      {album.title}
-    </div>
+    <Link to={`/artists/${album.artist_id}/albums/${album.id}`}>
+      <img className = 'album-index-item-image'
+        src={album.album_picture_url} />
+      <div className = 'album-index-item-title'>
+          <span>{album.title}</span>
+      </div>
+    </Link>
   </li>
 );
 
--- frontend/components/artist/artist_index_item.jsx
+++ frontend/components/artist/artist_index_item.jsx
@@ -5,10 +5,11 @@ const ArtistIndexItem = ({artist}) => (
   <li className = 'artist-index-item'>
     <Link to={`/artists/${artist.id}`} ><img className = 'artist-index-item-image'
       src={artist.artist_picture_url}
-    /></Link>
-    <div className = 'artist-index-item-name'>
-      {artist.name}
-    </div>
+    />
+  <div className = 'artist-index-item-name'>
+        <span>{artist.name}</span>
+      </div>
+    </Link>
   </li>
 );
 
